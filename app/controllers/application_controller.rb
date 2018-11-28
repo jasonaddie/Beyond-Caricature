fd57d72a381a2192f2da05d50b7cb58db7855a73
jason <jason.addie@gmail.com>
2018-11-28 14:55:53 +0400

    add locale to url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
 
   before_action :set_paper_trail_whodunnit
+
+  #################
+  ## LOCALES ##
+  #################
+  # make sure the locale is set if it does not exist in the url
+  before_action :set_locale
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+  # make sure locale is in url
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  devise_for :users
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
 
-  root to: "home#index"
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+    devise_for :users
+
+
+
+    # default homepage
+    root to: "home#index"
+
+    # if there is no match, redirect to default locale home page
+    match "*path", to: redirect("/#{I18n.default_locale}"), via: :all
+  end
+
+  match '', to: redirect("/#{I18n.default_locale}"), via: :all # handles /
+  match '*path', to: redirect("/#{I18n.default_locale}/%{path}"), via: :all # handles /not-a-locale/anything
+
 end
