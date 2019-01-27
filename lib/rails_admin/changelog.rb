a5573cd7e77e60e73aa280753148e5acde7b19d7
jason <jason.addie@gmail.com>
2019-01-27 23:02:26 +0400

    added current version from changelog to sidebar nav

--- lib/rails_admin/changelog.rb
+++ lib/rails_admin/changelog.rb
@@ -10,32 +10,9 @@ module RailsAdmin
 
         register_instance_option :controller do
           proc do
-            file = Rails.root.join("CHANGELOG.md")
-            cache_key = [file, File.mtime(file)].join('-')
 
-            # taken from here: https://richonrails.com/articles/rendering-markdown-with-redcarpet
-            @changelog_html = Rails.cache.fetch(cache_key, expires_in: 12.hours) do
-              options = {
-                filter_html:     true,
-                hard_wrap:       true,
-                link_attributes: { rel: 'nofollow', target: "_blank" },
-                space_after_headers: true,
-                fenced_code_blocks: true
-              }
-
-              extensions = {
-                autolink:           true,
-                superscript:        true,
-                disable_indented_code_blocks: true
-              }
-
-              renderer = Redcarpet::Render::HTML.new(options)
-              markdown = Redcarpet::Markdown.new(renderer, extensions)
-
-              markdown.render(File.read(file)).html_safe
-            end
-
-            # @changelog_html = get_changelog_html
+            # this calls a method in application helper
+            @changelog_html = get_changelog_as_html
 
             respond_to do |format|
               format.html { render @action.template_name }
