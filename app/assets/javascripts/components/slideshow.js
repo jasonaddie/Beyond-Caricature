b9680fe078155a8f26d63c5126ccc52c5e71904d
jason <jason.addie@gmail.com>
2019-04-07 00:38:14 +0400

    changed slideshow library

--- app/assets/javascripts/components/slideshow.js
+++ app/assets/javascripts/components/slideshow.js
@@ -1,27 +1,25 @@
-//= require jssor.slider.min
+//= require jquery-3.3.1.min
+//= require slick.min
+
 document.addEventListener("turbolinks:load", function() {
 
   // news item slideshow
-  if (document.querySelector('#news-item-slideshow') !== null){
-    var options = {
-      $AutoPlay: 1,
-      $Idle: 5000,
-      $FillMode : 1,
-      $SlideSpacing: 65,
-      $ArrowNavigatorOptions: {
-        $Class: $JssorArrowNavigator$
-      },
-      $BulletNavigatorOptions: {
-        $Class: $JssorBulletNavigator$
-      }
-    }
-    var jssor1_slider = new $JssorSlider$("news-item-slideshow", options)
+  if (document.querySelector('.slideshow-news-item-slick .slideshow-slides') !== null){
+    $('.slideshow-news-item-slick .slideshow-slides').slick({
+      centerMode: true,
+      autoplay: false,
+      autoplaySpeed: 4000,
+      variableWidth: true,
+      arrows: true,
+      nextArrow: $('.slick-next'),
+      prevArrow: $('.slick-prev')
+    });
 
     // if the news content has the placeholder [slideshow] in it move the slideshow to this spot
     var p = document.querySelectorAll('.news-item-container .news-item-content p')
     for (var i=0;i<p.length;i++){
       if (p[i].textContent.includes('[slideshow]')){
-        document.querySelector('.news-item-container .news-item-content').replaceChild(document.querySelector('#news-item-slideshow'), p[i])
+        document.querySelector('.news-item-container .news-item-content').replaceChild(document.querySelector('.slideshow-news-item-slick'), p[i])
         break
       }
     }
