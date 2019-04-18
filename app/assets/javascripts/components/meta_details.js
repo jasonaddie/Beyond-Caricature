d5a9367806a2bf7f435161e861359feab6f1c508
jason <jason.addie@gmail.com>
2019-04-19 00:18:01 +0400

    got resize to work with meta text more link

--- app/assets/javascripts/components/meta_details.js
+++ app/assets/javascripts/components/meta_details.js
@@ -11,22 +11,23 @@ document.addEventListener("turbolinks:load", function() {
     if (container !== null){
       var img = container.querySelector('.meta-image img')
       var title = container.querySelector('h1.title')
-      var meta_internal = container.querySelector('.meta-items-details')
+      var list = container.querySelector('.meta-items-list')
       var text = container.querySelector('.meta-text')
       var more = container.querySelector('.meta-text-more')
       var more_modal = document.querySelector('body.meta-details-container .modal-meta-text')
+      var meta_height = title.scrollHeight + list.scrollHeight + text.scrollHeight
 
-
-      if (meta_internal.scrollHeight > img.scrollHeight){
+      // console.log(img.scrollHeight)
+      // console.log(title.scrollHeight)
+      // console.log(list.scrollHeight)
+      // console.log(text.scrollHeight)
+      // console.log(meta_height)
+      // console.log(img.scrollHeight - (meta_height - text.scrollHeight))
+      // console.log('==')
+      if (meta_height > img.scrollHeight){
         // set text height to match img height
         // console.log('- need to adjust')
-        // console.log(text.style.height)
-        // console.log(img.scrollHeight)
-        // console.log(meta.scrollHeight)
-        // console.log(text.scrollHeight)
-        // console.log(img.scrollHeight - (meta.scrollHeight - text.scrollHeight))
-        // console.log('==')
-        text.style.height = (img.scrollHeight - (meta_internal.scrollHeight - text.scrollHeight) - title.scrollHeight - 50 - 30) + 'px'
+        text.style.height = (img.scrollHeight - (meta_height - text.scrollHeight) - title.scrollHeight - 50 - 30) + 'px'
         // console.log('-')
         // console.log(text.style.height)
 
@@ -40,10 +41,7 @@ document.addEventListener("turbolinks:load", function() {
       }
     }
   }
-
-  // window.addEventListener("resize", checkPortraitMetaTextOverflow)
-  // $(window).on('resize', checkPortraitMetaTextOverflow)
-
+  $(window).on('resize', debounce(checkPortraitMetaTextOverflow, 250))
   $('body.meta-details-container .meta-details.portrait .meta-image img').on('load', checkPortraitMetaTextOverflow)
 
 
@@ -66,16 +64,20 @@ document.addEventListener("turbolinks:load", function() {
         li_height += li.scrollHeight
       })
 
+      // console.log(text.style.height)
+      // console.log(li_height)
+      // console.log(text.scrollHeight)
+      // console.log('==')
       if (text.scrollHeight > li_height){
         // set text height to match img height
         // console.log('- need to adjust')
-        // console.log(text.style.height)
-        // console.log(img.scrollHeight)
-        // console.log(meta.scrollHeight)
-        // console.log(text.scrollHeight)
-        // console.log(img.scrollHeight - (meta.scrollHeight - text.scrollHeight))
-        // console.log('==')
-        text.style.height = (li_height - 50 - 30) + 'px'
+        var new_height = li_height - 50
+        // console.log(new_height)
+        if (new_height < 200){
+          // must be at least 200px hight
+          new_height = 200
+        }
+        text.style.height = new_height + 'px'
         // console.log('-')
         // console.log(text.style.height)
 
@@ -89,9 +91,7 @@ document.addEventListener("turbolinks:load", function() {
       }
     }
   }
-
-  // window.addEventListener("resize", checkLandscapeMetaTextOverflow)
-  // $(window).on('resize', checkLandscapeMetaTextOverflow)
+  $(window).on('resize', debounce(checkLandscapeMetaTextOverflow, 250))
   checkLandscapeMetaTextOverflow()
 
 
