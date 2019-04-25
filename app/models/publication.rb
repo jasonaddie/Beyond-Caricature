5193e99aa3b4b812259f6e99c15508cbdeae4a36
jason <jason.addie@gmail.com>
2019-04-26 00:03:44 +0400

    bug fix - update view pdf i18n text

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -217,7 +217,9 @@ class Publication < ApplicationRecord
       pretty_value do
         if bindings[:object].scanned_file.present?
           bindings[:view].content_tag(:a,
-            I18n.t('labels.view_file_with_size', size: ActionController::Base.helpers.number_to_human_size(bindings[:object].scanned_file_size)),
+            I18n.t('labels.view_file_with_size',
+                    size: ActionController::Base.helpers.number_to_human_size(bindings[:object].scanned_file_size),
+                    name: bindings[:object].publication_type_formatted),
             href: bindings[:object].scanned_file.url,
             target: '_blank',
             class: 'btn btn-info btn-sm'
