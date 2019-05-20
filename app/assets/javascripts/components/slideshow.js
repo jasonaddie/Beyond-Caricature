f9ac41ae1c390edaf315c36f6e3e9bf0995ade2c
jason <jason.addie@gmail.com>
2019-05-20 18:06:43 +0400

    forgot to reset slideshow settings value

--- app/assets/javascripts/components/slideshow.js
+++ app/assets/javascripts/components/slideshow.js
@@ -5,7 +5,7 @@ document.addEventListener("turbolinks:load", function() {
   // highlights slideshow
   if (document.querySelector('.slideshow-highlights-slick .slideshow-slides') !== null){
     $('.slideshow-highlights-slick .slideshow-slides').slick({
-      autoplay: false,
+      autoplay: true,
       autoplaySpeed: 4000,
       variableWidth: false,
       arrows: false,
