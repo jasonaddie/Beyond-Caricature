b4000a242ad857687a0a182f81d7558e31fb5fa5
jason <jason.addie@gmail.com>
2019-05-21 22:11:52 +0400

    updated source page roles so they are better grouped together

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -109,4 +109,63 @@ module ApplicationHelper
     end
   end
 
+
+  # if the publication is a journal, there may be publication roles
+  # and if there are, these roles have years assigned to them,
+  # so we then have to see if the role also exists in publication roles
+  # and if so, they need to be merged
+  # return format: { Role: { Unknown: [person, person, person], 1920: [person] } }
+  def merge_publication_roles(publication, publication_editors)
+    roles = {}
+
+    if publication.present?
+      publication_roles = publication.person_roles.group_people_by_role
+
+      # add publication roles
+      if publication_roles.present?
+        unknown_key = I18n.t('labels.unknown')
+        publication_roles.each do |role, people|
+          roles[role] = {}
+          roles[role][unknown_key] = []
+          people.each do |person|
+            roles[role][unknown_key] << person
+          end
+        end
+      end
+
+      # add publication editor roles
+      if publication_editors.present?
+        publication_editors.each do |publication_editor|
+          pe_roles = publication_editor.person_roles.group_people_by_role
+          # create the hash key to put the role data into
+          # if the pub editor record has no dates, use the unknown key
+          # else use the start and end date to create the hash key
+          date_key = if publication_editor.year_start.present? || publication_editor.year_end.present?
+            (publication_editor.year_start ? publication_editor.year_start.to_s : '?') +
+            ' - ' +
+            (publication_editor.year_end ? publication_editor.year_end.to_s : '?')
+          else
+            unknown_key
+          end
+
+          if pe_roles.present?
+            pe_roles.each do |pe_role, people|
+              if !roles[pe_role].present?
+                roles[pe_role] = {}
+              end
+              if !roles[pe_role][date_key].present?
+                roles[pe_role][date_key] = []
+              end
+
+              people.each do |person|
+                roles[pe_role][date_key] << person
+              end
+            end
+          end
+        end
+      end
+    end
+
+    return roles
+  end
 end
